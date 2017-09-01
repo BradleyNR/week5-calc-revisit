6c5d60380e18ba79e8b1dd9e3ee452a6343661a3
Bradley_Carson <bmcarson1671@gmail.com>
2017-09-01 16:16:07 -0400

    finished

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,12 +3,12 @@ let buttons = ['7', '8', '9', '/', '4', '5', '6', '*',
  '1', '2', '3', '-', '0', '.', '=', '+'];
 let appendTo = document.querySelector('.calculator-section');
 let storageArray = [];
-let operators = ['+', '-', '*', '/']
+let operators = ['+', '-', '*', '/'];
 let answerArea = document.querySelector('.answer-area');
 
-
-console.log(typeof parseInt(buttons[1]));
-
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 //create buttons
 buttons.forEach(function(button, index){
@@ -16,7 +16,7 @@ buttons.forEach(function(button, index){
   let equal = '=';
   //sets classes correctly based on content of button
   if (operators.includes(button)) {
-    clickers.className = "operators-class";
+    clickers.className = 'operators-class';
   } else if (equal.includes(button)) {
     clickers.className = 'equal-class';
   } else {
@@ -30,53 +30,59 @@ buttons.forEach(function(button, index){
   //onclick
   clickers.addEventListener('click', function(e){
     console.log(clickers.textContent);
-    //checks to see if operators are already in array and early returns if they are
-    if (operators.includes(clickers.textContent) && (storageArray.includes('+')
-    || storageArray.includes('-') || storageArray.includes('*') || storageArray.includes('/'))) {
-      return
-    } else if (equal.includes(clickers.textContent)) {
-      return
-    } else {
-      storageArray += clickers.textContent;
+    if ((operators+['=']).includes(clickers.textContent)) {
+      storageArray.push(answerArea.textContent);
+      storageArray.push(clickers.textContent);
+      answerArea.textContent = clickers.textContent;
+    } else if (isNumeric(clickers.textContent)) {
+      console.log('numeric');
+      if (!isNumeric(answerArea.textContent)) {
+        answerArea.textContent = clickers.textContent;
+      } else {
+        answerArea.textContent += clickers.textContent;
+      }
     }
     console.log(storageArray);
-
-    answerArea.textContent = storageArray;
   });
 });
 
 let equalButton = document.querySelector('.equal-class');
 
 equalButton.addEventListener('click', function(e){
+  console.log('Storage Area ', storageArray);
+  let result = parseFloat(storageArray[0]);
+
   //MATH BELOW BEWARE
-  if (storageArray.includes('+')) {
-    storageArray = storageArray.split('+');
-    let answer = parseFloat(storageArray[0]) + parseFloat(storageArray[1]);
-    storageArray[0] = answer;
-    answerArea.textContent = answer;
-  } else if (storageArray.includes('-')){
-    storageArray = storageArray.split('-');
-    let answer = parseFloat(storageArray[0]) - parseFloat(storageArray[1]);
-    storageArray[0] = answer;
-    answerArea.textContent = answer;
-  } else if (storageArray.includes('/')) {
-    storageArray = storageArray.split('/');
-    let answer = parseFloat(storageArray[0]) / parseFloat(storageArray[1]);
-    storageArray[0] = answer;
-    answerArea.textContent = answer;
-  } else if (storageArray.includes('*')) {
-    storageArray = storageArray.split('*');
-    let answer = parseFloat(storageArray[0]) * parseFloat(storageArray[1]);
-    storageArray[0] = answer;
-    answerArea.textContent = answer;
+  for(var i = 1; i < storageArray.length; i += 2){
+    let operator = storageArray[i];
+    let secondNumber = parseFloat(storageArray[i + 1]);
+
+    switch (operator) {
+      case '+':
+        result += secondNumber;
+        break;
+      case '-':
+        result -= secondNumber;
+      case '*':
+        result *= secondNumber;
+      case '/':
+        result /= secondNumber;
+      default:
+    }
   }
-  console.log(storageArray);
-  storageArray[1] = [];
+
+  console.log(result);
+  answerArea.textContent = result;
+  storageArray = [];
 });
 
-let clearButton = document.querySelector('.clearbutton')
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+let clearButton = document.querySelector('.clearbutton');
 
 clearButton.addEventListener('click', function(){
-  answerArea.textContent = 'clear';
+  answerArea.textContent = '';
   storageArray = [];
 });
